beaa61ecfd6a6b1abb5aa680c62bdd7a673d5d2f
Nicolás Salas V <nikosalas@gmail.com>
2019-12-17 21:41:18 -0300

    Include Devise helpers for RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,9 +36,10 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
+  # Devise for RSpec
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+
+  # Each spec runs inside a transaction
   config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
