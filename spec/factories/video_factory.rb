e02797dc5702d1fa74e51b4ed52140d79c5ba8b0
Nicolás Salas V <nikosalas@gmail.com>
2019-12-17 22:03:35 -0300

    Add factories for admins and videos

---
+++ spec/factories/video_factory.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+FactoryBot.define do
+  factory :video do
+    name { Faker::Movie.quote }
+
+    trait :with_image do
+      file {
+        fixture_file_upload(
+          Rails.root.join('spec', 'factories', 'assets', 'pear.jpg'),
+          'image/jpeg'
+        )
+      }
+    end
+  end
+end
